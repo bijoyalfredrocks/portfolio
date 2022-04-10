128ea5b89f558c775ab7f520f942926269034bd5
bijoyalfredrocks <bijoyalfred1983@gmail.com>
2022-04-10 20:22:41 +0530

    nav bar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home";
+import Demo from "./pages/Demo";
+import KeyFeatures from "./pages/KeyFeatures";
+import Pricing from "./pages/Pricing";
+import Testimonials from "./pages/Testimonials";
+
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hello World</h1>
+    <div className="container">
+     
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/features" exact component={KeyFeatures} />
+          <Route path="/pricing" exact component={Pricing} />
+          <Route path="/testimonials" exact component={Testimonials} />
+          <Route path="/demo" exact component={Demo} />
+          </Routes>
+      </Router>
+      
     </div>
   );
 }
